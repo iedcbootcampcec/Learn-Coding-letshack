bf7533c5ecfde05668f582ce058074544103b78b
Haaryy <64596581+Haaryy@users.noreply.github.com>
2021-10-03 22:25:19 +0530

    Create haaryy.c

---
+++ C/haaryy.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Haaritha\n");
+     printf("Age    : 19\n");
+     printf("Username : Haaryy\n");
+     return(0);
+  }
