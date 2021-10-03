e7b17ab5d10e7c5fc73e72f0598a88077846a456
Haaryy <64596581+Haaryy@users.noreply.github.com>
2021-10-03 22:27:12 +0530

    Create haaryy.java

---
+++ JAVA/haaryy.java
@@ -0,0 +1,10 @@
+class Haaryy {
+    public static void main(String[] args) {
+
+    String name = "Haaritha";
+    int age = 19 ;
+    String username = "Haaryy";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
