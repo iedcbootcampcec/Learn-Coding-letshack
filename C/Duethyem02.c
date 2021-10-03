1a6570035e9cfb9fe695d05bce9564d62fe7e742
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2021-10-03 21:54:25 +0530

    Created Duethyem02.c

---
+++ C/Duethyem02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : DUETHY ELSA MATHEWS\n");
+     printf("Age    : 19\n");
+     printf("Username : Duethyem02\n");
+     return(0);
+  } 
