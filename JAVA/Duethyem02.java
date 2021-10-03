0a2585462eefdfa441bcbdb00e5f231b75a41158
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2021-10-03 22:43:29 +0530

    Create Duethyem02.java

---
+++ JAVA/Duethyem02.java
@@ -0,0 +1,10 @@
+class Duethyem02 {
+    public static void main(String[] args) {
+
+    String name = "Duethy Elsa Mathews";
+    int age = 19 ;
+    String username = "Duethyem02";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
