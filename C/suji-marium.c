ee990b7eaa595bc1076ba593fdc4ddac1ced466a
suji-marium <85631667+suji-marium@users.noreply.github.com>
2021-10-04 16:31:57 +0530

    Create suji-marium.c

---
+++ C/suji-marium.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : SUJI MARIUM SAJI\n");
+     printf("Age    : 20\n");
+     printf("Username : suji-marium\n");
+     return(0);
+  }
