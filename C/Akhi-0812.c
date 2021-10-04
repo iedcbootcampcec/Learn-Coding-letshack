f2b6f2f9e2697c53e0394fc81a2ac43267601190
Akhi-0812 <73642930+Akhi-0812@users.noreply.github.com>
2021-10-04 17:21:42 +0530

    Create Akhi-0812.c

---
+++ C/Akhi-0812.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Akhi P Venu\n");
+     printf("Age    : 20\n");
+     printf("Username : Akhi-0812\n");
+     return(0);
+  }
