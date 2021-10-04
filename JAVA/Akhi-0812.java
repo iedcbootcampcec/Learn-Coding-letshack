d688cc6135842d16a4750cde798b8ef82e5a0a79
Akhi-0812 <73642930+Akhi-0812@users.noreply.github.com>
2021-10-04 17:33:49 +0530

    Create Akhi-0812.java

---
+++ JAVA/Akhi-0812.java
@@ -0,0 +1,10 @@
+class Akhi-0812 {
+    public static void main(String[] args) {
+
+    String name = "Akhi P Venu";
+    int age = 20 ;
+    String username = "Akhi-0812";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
