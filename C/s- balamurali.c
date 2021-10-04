e161cd1b87dfb5efaaec3707a675965253404862
S Balamurali <71454278+s-balamurali@users.noreply.github.com>
2021-10-04 09:17:00 +0530

    Create s- balamurali.c

---
+++ C/s- balamurali.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : S Balamurali\n");
+     printf("Age    : 21\n");
+     printf("Username : s-balamurali\n");
+     return(0);
+  }
