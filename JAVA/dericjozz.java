48b03dcf75ad8c7653000c31e69855da01be2f5d
dericjozz <91784564+dericjozz@users.noreply.github.com>
2021-10-04 13:53:24 +0530

    Create dericjozz.java

---
+++ JAVA/dericjozz.java
@@ -0,0 +1,10 @@
+class dericjozz {
+    public static void main(String[] args) {
+
+    String name = "DERIC JOSE";
+    int age = 21 ;
+    String username = "dericjozz";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
