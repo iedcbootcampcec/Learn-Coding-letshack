7ce84cec6b5c4160d4c6237b6b4be56759ff27ca
suji-marium <85631667+suji-marium@users.noreply.github.com>
2021-10-04 16:43:38 +0530

    Create suji-marium.java

---
+++ JAVA/suji-marium.java
@@ -0,0 +1,10 @@
+class suji-marium {
+    public static void main(String[] args) {
+
+    String name = "SUJI MARIUM SAJI";
+    int age = 20 ;
+    String username = "suji-marium";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
