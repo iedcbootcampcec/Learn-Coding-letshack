cbfd71faf25ce0dc2ac162beccfdc964b060bf6c
Aiswarya Sankar <71959577+aiswaryasankar23@users.noreply.github.com>
2021-10-05 21:38:31 +0530

    Create aiswarya.java

---
+++ JAVA/aiswarya.java
@@ -0,0 +1,10 @@
+class aiswarya {
+    public static void main(String[] args) {
+
+    String name = "aiswarya";
+    int age =21 ;
+    String username = "aiswarya23";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
