f430ff7dce1824ef572e887c97fea34c4e1ecc4c
Aiswarya Sankar <71959577+aiswaryasankar23@users.noreply.github.com>
2021-10-05 21:26:34 +0530

    Create aiswarya.c

---
+++ C/aiswarya.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : AISWARYA\n");
+     printf("Age    : 21\n");
+     printf("Username : aiswarya23\n");
+     return(0);
+  }
