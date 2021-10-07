a9aaded41853b18ecffdef9712e21c3aac1ccf06
Akash S <akashsureshmyd@gmail.com>
2021-10-07 11:07:05 +0530

    Added intro to programming languages

---
+++ JAVA/akash.java
@@ -0,0 +1,10 @@
+class Akash {
+    public static void main(String[] args) {
+
+    String name = "Akash S";
+    int age = 19 ;
+    String username = "akkupy";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
