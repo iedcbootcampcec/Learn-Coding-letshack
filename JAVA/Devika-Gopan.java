44bb3674136b84b8b6b4e641aa23b1ae7059892b
Devika Gopan <73544738+Devika-Gopan@users.noreply.github.com>
2021-10-09 00:42:28 +0530

    Create Devika-Gopan.java

---
+++ JAVA/Devika-Gopan.java
@@ -0,0 +1,10 @@
+class Devika-Gopan{
+    public static void main(String[] args) {
+
+    String name = "Devika Gopan";
+    int age = 21 ;
+    String username = "Devika-Gopan";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
