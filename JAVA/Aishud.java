3774abba0490fff1fd78c884d00a7dd08143ded1
Aishud <77443206+Aishud@users.noreply.github.com>
2021-10-16 18:27:32 +0530

    Create Aishud.java

---
+++ JAVA/Aishud.java
@@ -0,0 +1,10 @@
+class Aishud {
+    public static void main(String[] args) {
+
+    String name = "Aiswarya das";
+    int age = 19 ;
+    String username = "Aishud";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
